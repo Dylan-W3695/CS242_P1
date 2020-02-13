ee923b8318ebd8619e5516dbb67b9460e0d08e3d
Dylan-W3695 <wahl_d@lynchburg.edu>
2020-02-13 14:18:46 -0500

    console interface started

--- src/songs/ConsoleInterface.java
+++ src/songs/ConsoleInterface.java
@@ -1,4 +1,7 @@
 package songs;
+
+import java.lang.String;
+import java.util.Comparator;
 /**
  * 
  * @author Wahl_D
@@ -7,8 +10,23 @@ package songs;
 public abstract class ConsoleInterface {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Comparator cmp = null;
+		switch(args[0]) {
+			case "0":
+				cmp = new NameComparator();
+				break;
+			case "1":
+				cmp = new AlbumComparator();
+				break;
+			case "2":
+				cmp = new ArtistComparator();
+				break;
+		}
+		ULSortedList<Song> songList = new ULSortedList<Song>(cmp);
+		
+		
+		
 	}
 
 }
